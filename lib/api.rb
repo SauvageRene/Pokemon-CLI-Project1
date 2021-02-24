fd632cdf07a6ff6b173a5e089b6776234fac252f
Rene <renegarcia1864@gmail.com>
2021-02-24 16:43:59 -0600

    change initialize method

--- lib/api.rb
+++ lib/api.rb
@@ -5,9 +5,10 @@ class API
     def self.grab_pokemon
         pokedex = 'https://pokeapi.co/api/v2/pokemon/'
         data_hash = HTTParty.get(pokedex)
-        array_of_data = pokedex["results"]
+        array_of_data = data_hash["results"]
         array_of_data.each do |name|
             Pokemon.new(name)
         end
     end
+    binding.pry
 end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,7 +6,8 @@ class Pokemon
 
     def initialize(attr_hash) # the hash assigns values to keys so values can be looked up by their key
         attr_hash.each do |key, value|
-            self.send("#{key}=", v) if self.respond_to?("#{key}=")
+            self.class.attr_accessor()
+            self.send("#{key}=", value)
         end
         save #binding.pry
     end
