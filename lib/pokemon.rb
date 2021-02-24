c8c869035d15c01fbc190b486f8839a6feb91c9e
Rene <renegarcia1864@gmail.com>
2021-02-24 15:04:07 -0600

    added all array, save method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,12 +2,20 @@ require 'pry'
 
 class Pokemon
     
-
+@@all = []
 
     def initialize(attr_hash) # the hash assigns values to keys so values can be looked up by their key
         attr_hash.each do |key, value|
             self.send("#{key}=", v) if self.respond_to?("#{key}=")
         end
-        binding.pry
+        #binding.pry
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
     end
 end
