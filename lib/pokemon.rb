c2d1b632e07c05dab39d1255b9c3f177eacd7399
Rene <renegarcia1864@gmail.com>
2021-02-24 14:51:29 -0600

    attr_hash added

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,6 +2,12 @@ require 'pry'
 
 class Pokemon
     
-    def initialize
+
+
+    def initialize(attr_hash) # the hash assigns values to keys so values can be looked up by their key
+        attr_hash.each do |key, value|
+            self.send("#{key}=", v) if self.respond_to?("#{key}=")
+        end
+        binding.pry
     end
 end
