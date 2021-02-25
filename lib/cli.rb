9d3a054a59e8fd9ffa259064346e8091bc5ccb94
Rene <renegarcia1864@gmail.com>
2021-02-25 16:34:50 -0600

    input for menu modified

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,12 +5,21 @@ class CLI
     def run
         API.grab_pokemon
         puts "Welcome PokeMaster!"
-        puts "If you want a list of pokemon availble in PoGo enter 'Yes'"
+        puts "If you want a list of pokemon availble in PoGo enter 'yes'"
         puts "To exit the application, enter 'exit'"
+        menu
         binding.pry
     end
 
     def menu
+        input = gets.chomp
+        if input == "yes"
+            pokemon_list
+        elsif input == "exit"
+            goodbye
+        else
+            invalid_entry
+        end
     end
 
     def input_to_index(input)
