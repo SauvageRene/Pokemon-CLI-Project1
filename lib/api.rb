ac03b4dbcb96a5366c2170350bd905da3ecb5b69
Rene <renegarcia1864@gmail.com>
2021-02-24 19:54:59 -0600

    fixing API_URL

--- lib/api.rb
+++ lib/api.rb
@@ -3,8 +3,8 @@ require 'pry'
 class API 
     
     def self.grab_pokemon
-        URL = "https://pogoapi.net/api/v1/pokemon_rarity.json"
-        data_hash = HTTParty.get(URL)
+        API_URL = "https://pogoapi.net/api/v1/pokemon_stats.json"
+        response = HTTParty.get(API_URL)
         array_of_data = data_hash
         array_of_data.each do |name|
             Pokemon.new(name)
